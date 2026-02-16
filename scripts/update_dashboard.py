f75e145b6cb0f7547e1823fc549f5495581c365f
Himanshu Pandey <himanshupandey0410@gmail.com>
2026-02-16 14:20:42 +0530

    fix: replace broken animated GIFs with reliable Fluent Emojis in table header

--- scripts/update_dashboard.py
+++ scripts/update_dashboard.py
@@ -125,16 +125,15 @@ def generate_language_bar(languages):
 
 def generate_markdown(projects):
     # Professional HTML Container Table with "Terminal" aesthetic
-    base_asset_url = "https://raw.githubusercontent.com/himanshupandey04/himanshupandey04/main/assets/animated_icons"
     
     html_output = '<table width="100%" style="border-collapse: collapse; background-color: #0d1117; border: 1px solid #30363d; color: #e6edf3;">\n'
     
     # Header Row with Animated Icons
     html_output += '<thead style="background-color: #161b22; border-bottom: 1px solid #30363d;"><tr>'
-    html_output += f'<th width="20%" align="left" style="padding: 10px;"><img src="{base_asset_url}/coding.gif" height="25" style="vertical-align: middle;" />&nbsp; PROJECT MODULE</th>'
-    html_output += f'<th width="45%" align="left" style="padding: 10px;"><img src="{base_asset_url}/brain.gif" height="25" style="vertical-align: middle;" />&nbsp; INTELLIGENCE / DESC</th>'
-    html_output += f'<th width="25%" align="left" style="padding: 10px;"><img src="{base_asset_url}/data.gif" height="25" style="vertical-align: middle;" />&nbsp; COMPOSITION (100%)</th>'
-    html_output += f'<th width="10%" align="center" style="padding: 10px;"><img src="{base_asset_url}/server.gif" height="25" style="vertical-align: middle;" />&nbsp; ACCESS</th>'
+    html_output += f'<th width="20%" align="left" style="padding: 10px;"><img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/People/Technologist.png" height="25" style="vertical-align: middle;" />&nbsp; PROJECT MODULE</th>'
+    html_output += f'<th width="45%" align="left" style="padding: 10px;"><img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Objects/Light%20Bulb.png" height="25" style="vertical-align: middle;" />&nbsp; INTELLIGENCE / DESC</th>'
+    html_output += f'<th width="25%" align="left" style="padding: 10px;"><img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Objects/Chart%20Increasing.png" height="25" style="vertical-align: middle;" />&nbsp; COMPOSITION (100%)</th>'
+    html_output += f'<th width="10%" align="center" style="padding: 10px;"><img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Travel%20and%20places/Rocket.png" height="25" style="vertical-align: middle;" />&nbsp; ACCESS</th>'
     html_output += '</tr></thead>\n<tbody>\n'
     
     for p in projects:
